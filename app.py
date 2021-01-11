6d96e97dd0230b30746dcd4ce7ba4fea1798c932
choi-jinah <choi-jinah@lotte.net>
2021-01-11 15:55:29 +0900

    템플릿 생성

---
+++ app.py
@@ -0,0 +1,17 @@
+# 실행 시 서버가 가동될 파이썬 파일
+
+from flask import Flask, render_template
+
+# Flask 객체 인스턴스 생성
+app = Flask(__name__)
+
+
+@app.route('/')  # 접속하는 url
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True) # 서버 가동
+    # host 지정
+    # app.run(host="127.0.0.1", port="5000", debug=True)
